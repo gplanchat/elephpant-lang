e1902dcf464bcc1c9526492f1c0308f343b1bc06
Grégory Planchat <gregory@luni.fr>
2015-03-22 04:39:07 +0100

    Enforced operator calling
    Signed-Off-By: Grégory Planchat<gregory@luni.fr>

--- lang/standard.cpp
+++ lang/standard.cpp
@@ -11,105 +11,122 @@ using namespace std;
 using namespace rephp::engine;
 
 template<typename T1, typename T2>
-bool scalar_worker_add<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_add<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(cur + operand);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left + right);
+
+    return return_value;
 }
 
 template<typename T1, typename T2>
-bool
-scalar_worker_sub<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_sub<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(cur - operand);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left - right);
+
+    return return_value;
 }
 
 template<typename T1, typename T2>
-bool
-scalar_worker_mul<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_mul<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(cur * operand);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left * right);
+
+    return return_value;
 }
 
 template<typename T1, typename T2>
-bool
-scalar_worker_div<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_div<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(cur / operand);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left / right);
+
+    return return_value;
 }
 
 template<typename T1, typename T2>
-bool
-scalar_worker_mod<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_mod<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
-    auto result = std::div(cur, operand);
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(result.rem);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left % right);
+
+    return return_value;
 }
 
 template<typename T1, typename T2>
-bool
-scalar_worker_intdiv<T1, T2>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_intdiv<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
-    auto cur = self->get<T1>();
     if (parameters.size() != 1) {
-        return false;
+        rephp::engine::engine::throw_exception(nullptr, "Invalid operator call.");
+        return rephp::engine::engine::invoke("Null", 0);
     }
 
-    auto operand = parameters[0]->get<T2>();
-    auto result = std::div(cur, operand);
+    type::value_t return_value = self->copy();
 
-    return_value->set<T1>(result.quot);
+    auto left = self->get<T1>();
+    auto right = static_cast<T1>(parameters[0]->get<T2>());
 
-    return true;
+    return_value->set(left - right);
+
+    return return_value;
 }
-
+/*
 template<typename T>
-bool
-string_worker_offset_set<T>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_sub<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
     auto cur = self->get<T>();
     if (parameters.size() != 2) {
@@ -127,8 +144,8 @@ string_worker_offset_set<T>::operator() (type::value_t self, type::vector_t para
 }
 
 template<typename T>
-bool
-string_worker_offset_get<T>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_sub<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
     auto cur = self->get<T>();
     if (parameters.size() != 2) {
@@ -143,7 +160,8 @@ string_worker_offset_get<T>::operator() (type::value_t self, type::vector_t para
 }
 
 template<typename T>
-bool string_worker_add<T>::operator() (type::value_t self, type::vector_t parameters, type::value_t return_value)
+type::value_t
+scalar_worker_sub<T1, T2>::operator() (type::call_parameters_list_t parameters, type::value_t self)
 {
     auto cur = self->get<T>();
     if (parameters.size() > 1) {
@@ -158,7 +176,7 @@ bool string_worker_add<T>::operator() (type::value_t self, type::vector_t parame
 
     return true;
 }
-
+*/
 standard_bundle::standard_bundle(const char *const name): standard_bundle(type::string_t(name))
 {}
 
@@ -214,75 +232,72 @@ standard_bundle::standard_bundle(type::string_t name): bundle(name)
     worker_binary_operator_registration(ce_integer, OP_SUB, type::worker_t(new scalar_worker_sub<long, double>()), ce_float);
     worker_binary_operator_registration(ce_integer, OP_MUL, type::worker_t(new scalar_worker_mul<long, double>()), ce_float);
     worker_binary_operator_registration(ce_integer, OP_DIV, type::worker_t(new scalar_worker_div<long, double>()), ce_float);
-    worker_binary_operator_registration(ce_integer, OP_MOD, type::worker_t(new scalar_worker_mod<long, double>()), ce_float);
     worker_binary_operator_registration(ce_integer, OP_INTDIV, type::worker_t(new scalar_worker_intdiv<long, double>()), ce_float);
 
     worker_binary_operator_registration(ce_float, OP_ADD, type::worker_t(new scalar_worker_add<double, double>()), ce_float);
     worker_binary_operator_registration(ce_float, OP_SUB, type::worker_t(new scalar_worker_sub<double, double>()), ce_float);
     worker_binary_operator_registration(ce_float, OP_MUL, type::worker_t(new scalar_worker_mul<double, double>()), ce_float);
     worker_binary_operator_registration(ce_float, OP_DIV, type::worker_t(new scalar_worker_div<double, double>()), ce_float);
-    worker_binary_operator_registration(ce_float, OP_MOD, type::worker_t(new scalar_worker_mod<double, double>()), ce_float);
     worker_binary_operator_registration(ce_float, OP_INTDIV, type::worker_t(new scalar_worker_intdiv<double, double>()), ce_float);
 
     worker_binary_operator_registration(ce_float, OP_ADD, type::worker_t(new scalar_worker_add<double, long>()), ce_integer);
     worker_binary_operator_registration(ce_float, OP_SUB, type::worker_t(new scalar_worker_sub<double, long>()), ce_integer);
     worker_binary_operator_registration(ce_float, OP_MUL, type::worker_t(new scalar_worker_mul<double, long>()), ce_integer);
     worker_binary_operator_registration(ce_float, OP_DIV, type::worker_t(new scalar_worker_div<double, long>()), ce_integer);
-    worker_binary_operator_registration(ce_float, OP_MOD, type::worker_t(new scalar_worker_mod<double, long>()), ce_integer);
     worker_binary_operator_registration(ce_float, OP_INTDIV, type::worker_t(new scalar_worker_intdiv<double, long>()), ce_integer);
 
-    type::type_t ce_char(new internal_type("Char", TYP_NATIVE_TYPE, ce_mixed));
-    this->register_class(ce_char);
-
-    type::type_t ce_callable(new internal_type("Callable", TYP_NATIVE_TYPE, ce_mixed));
-    this->register_class(ce_callable);
-
-    type::type_t ce_composite(new internal_type("Composite", TYP_NATIVE_TYPE, ce_mixed));
-    this->register_class(ce_composite);
-
-    type::type_t ce_traversable(new internal_type("Traversable", TYP_INTERFACE));
-    ce_traversable->add_interface(ce_composite);
-    this->register_class(ce_traversable);
-
-    type::type_t ce_iterator(new internal_type("Iterator", TYP_INTERFACE));
-    ce_iterator->add_interface(ce_traversable);
-    this->register_class(ce_iterator);
-
-    type::type_t ce_iterator_aggregate(new internal_type("IteratorAggregate", TYP_INTERFACE));
-    ce_iterator_aggregate->add_interface(ce_traversable);
-    this->register_class(ce_iterator_aggregate);
-    ce_iterator_aggregate->add_method("getIterator", type::prototype_t(new callable_prototype(ce_iterator)), nullptr, ACC_PUBLIC);
-
-    type::type_t ce_countable(new internal_type("Countable", TYP_INTERFACE));
-    ce_countable->add_interface(ce_composite);
-    this->register_class(ce_countable);
-    ce_countable->add_method("count", type::prototype_t(new callable_prototype(ce_integer)), nullptr, ACC_PUBLIC);
-
-    type::type_t ce_sortable(new internal_type("Sortable", TYP_INTERFACE));
-    ce_sortable->add_interface(ce_composite);
-    this->register_class(ce_sortable);
-
-    type::type_t ce_sorter(new internal_type("Sorter", TYP_INTERFACE));
-    this->register_class(ce_sorter);
-    auto proto_sorter_sort = type::prototype_t(new callable_prototype(ce_null));
-    proto_sorter_sort->add_parameter("sortableCollection", ce_sortable);
-    ce_sorter->add_method("sort", proto_sorter_sort, nullptr, ACC_PUBLIC);
-
-    type::type_t ce_string(new internal_type("String", TYP_NATIVE_TYPE, ce_composite));
-    ce_string->add_interface(ce_traversable);
-    ce_string->add_interface(ce_countable);
-    ce_string->add_interface(ce_sortable);
-    this->register_class(ce_string);
-
-    auto proto_string_foo = type::prototype_t(new callable_prototype(ce_null));
-    proto_string_foo->add_parameter("param1", ce_integer);
-    proto_string_foo->add_parameter("param2", ce_string);
-    proto_string_foo->add_parameter("param3", ce_float, this->invoke("Float", 2.));
-    ce_string->add_method("sort", proto_string_foo, nullptr, ACC_PUBLIC);
-
-    worker_ternary_operator_registration(ce_string, OP_OFFSET_SET, type::worker_t(new string_worker_offset_set<type::string_t>()), ce_integer, ce_char);
-    worker_binary_operator_registration(ce_string, OP_OFFSET_GET, type::worker_t(new string_worker_offset_get<type::string_t>()), ce_integer);
-    worker_binary_operator_registration(ce_string, OP_DOT, type::worker_t(new string_worker_add<type::string_t>()), ce_string);
+//    type::type_t ce_char(new internal_type("Char", TYP_NATIVE_TYPE, ce_mixed));
+//    this->register_class(ce_char);
+//
+//    type::type_t ce_callable(new internal_type("Callable", TYP_NATIVE_TYPE, ce_mixed));
+//    this->register_class(ce_callable);
+//
+//    type::type_t ce_composite(new internal_type("Composite", TYP_NATIVE_TYPE, ce_mixed));
+//    this->register_class(ce_composite);
+//
+//    type::type_t ce_traversable(new internal_type("Traversable", TYP_INTERFACE));
+//    ce_traversable->add_interface(ce_composite);
+//    this->register_class(ce_traversable);
+//
+//    type::type_t ce_iterator(new internal_type("Iterator", TYP_INTERFACE));
+//    ce_iterator->add_interface(ce_traversable);
+//    this->register_class(ce_iterator);
+//
+//    type::type_t ce_iterator_aggregate(new internal_type("IteratorAggregate", TYP_INTERFACE));
+//    ce_iterator_aggregate->add_interface(ce_traversable);
+//    this->register_class(ce_iterator_aggregate);
+//    ce_iterator_aggregate->add_method("getIterator", type::prototype_t(new callable_prototype(ce_iterator)), nullptr, ACC_PUBLIC);
+//
+//    type::type_t ce_countable(new internal_type("Countable", TYP_INTERFACE));
+//    ce_countable->add_interface(ce_composite);
+//    this->register_class(ce_countable);
+//    ce_countable->add_method("count", type::prototype_t(new callable_prototype(ce_integer)), nullptr, ACC_PUBLIC);
+//
+//    type::type_t ce_sortable(new internal_type("Sortable", TYP_INTERFACE));
+//    ce_sortable->add_interface(ce_composite);
+//    this->register_class(ce_sortable);
+//
+//    type::type_t ce_sorter(new internal_type("Sorter", TYP_INTERFACE));
+//    this->register_class(ce_sorter);
+//    auto proto_sorter_sort = type::prototype_t(new callable_prototype(ce_null));
+//    proto_sorter_sort->add_parameter("sortableCollection", ce_sortable);
+//    ce_sorter->add_method("sort", proto_sorter_sort, nullptr, ACC_PUBLIC);
+//
+//    type::type_t ce_string(new internal_type("String", TYP_NATIVE_TYPE, ce_composite));
+//    ce_string->add_interface(ce_traversable);
+//    ce_string->add_interface(ce_countable);
+//    ce_string->add_interface(ce_sortable);
+//    this->register_class(ce_string);
+//
+//    auto proto_string_foo = type::prototype_t(new callable_prototype(ce_null));
+//    proto_string_foo->add_parameter("param1", ce_integer);
+//    proto_string_foo->add_parameter("param2", ce_string);
+//    proto_string_foo->add_parameter("param3", ce_float, this->invoke("Float", 2.));
+//    ce_string->add_method("sort", proto_string_foo, nullptr, ACC_PUBLIC);
+//
+//    worker_ternary_operator_registration(ce_string, OP_OFFSET_SET, type::worker_t(new string_worker_offset_set<type::string_t>()), ce_integer, ce_char);
+//    worker_binary_operator_registration(ce_string, OP_OFFSET_GET, type::worker_t(new string_worker_offset_get<type::string_t>()), ce_integer);
+//    worker_binary_operator_registration(ce_string, OP_DOT, type::worker_t(new string_worker_add<type::string_t>()), ce_string);
 }
 
 
