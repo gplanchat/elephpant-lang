9db6c767abffea7fcb1d3d6cef9c8dfe6a2b3d27
Grégory Planchat <gregory@luni.fr>
2015-03-22 04:50:28 +0100

    Updated README
    Signed-Off-By: Grégory Planchat<gregory@luni.fr>

--- main.cpp
+++ main.cpp
@@ -16,23 +16,20 @@ int main()
 
     engine::register_bundle(be_standard);
 
-    //rephp::reflection::bundle::dump(std::cout, be_standard);
+    rephp::reflection::bundle::dump(std::cout, be_standard) << std::endl;
 
-    auto a = be_standard->invoke("Integer", 8);
-    auto b = be_standard->invoke("Float", 2.);
-    auto c = be_standard->invoke("String", type::string_t("Hello!"));
+    auto a = engine::invoke("Integer", 8);
+    auto b = engine::invoke("Float", 2.);
+    auto c = engine::invoke("String", type::string_t("Hello!"));
     auto d = engine::invoke("String", type::string_t("Hello World!"));
 
-    //rephp::reflection::internal_value::dump(std::cout, a);
-    //rephp::reflection::internal_value::dump(std::cout, b);
-    //rephp::reflection::internal_value::dump(std::cout, c);
-    //rephp::reflection::internal_value::dump(std::cout, d) << std::endl;
+    rephp::reflection::internal_value::dump(std::cout, a) << std::endl;
+    rephp::reflection::internal_value::dump(std::cout, b) << std::endl;
 
     rephp::reflection::internal_value::dump(std::cout, (a + a)) << std::endl;
     rephp::reflection::internal_value::dump(std::cout, (a + b)) << std::endl;
-//    rephp::reflection::internal_value::dump(std::cout, a->call(rephp::engine::OP_ADD, {b})) << std::endl;
-//    rephp::reflection::internal_value::dump(std::cout, b->call(rephp::engine::OP_ADD, {b})) << std::endl;
-//    rephp::reflection::internal_value::dump(std::cout, b->call(rephp::engine::OP_ADD, {a})) << std::endl;
+    rephp::reflection::internal_value::dump(std::cout, (b + b)) << std::endl;
+    rephp::reflection::internal_value::dump(std::cout, (b + a)) << std::endl;
 
     //std::cout << a->call('toString') << std::endl;
     //std::cout << (a->call(rephp::engine::OP_ADD, {b}));
