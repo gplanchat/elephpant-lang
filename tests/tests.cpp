0127f83a59798c7fce8a1881a69278b6909af1b6
Grégory Planchat <gregory@luni.fr>
2015-03-28 03:57:39 +0100

    Fixed scalar testing
    Signed-Off-By: Grégory Planchat<gregory@luni.fr>

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,7 +9,6 @@ namespace test {
 test_suite &
 test_suite::operator<< (std::function<void()> test_case)
 {
-    std::cout << "[ " << this << " ] Adding test." << std::endl;
     test_cases.push_back(test_case);
 
     return *this;
@@ -18,9 +17,7 @@ test_suite::operator<< (std::function<void()> test_case)
 test_suite &
 test_suite::operator() ()
 {
-    std::cout << "[ " << this << " ] Running tests" << std::endl;
     for (auto it = test_cases.begin(); it != test_cases.end(); ++it) {
-        std::cout << "[ " << this << " ] Testing..." << std::endl;
         (*it)();
     }
 
