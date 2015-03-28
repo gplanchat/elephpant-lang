b677223c10477bd2739f06ccbbbcfc0094f0934d
Grégory Planchat <gregory@luni.fr>
2015-03-28 02:43:17 +0100

    Fixed testing architecture
    Signed-Off-By: Grégory Planchat<gregory@luni.fr>

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,7 +7,7 @@ namespace rephp {
 namespace test {
 
 test_suite &
-test_suite::operator<< (std::function<void()> &test_case)
+test_suite::operator<< (std::function<void()> test_case)
 {
     test_cases.push_back(test_case);
 
